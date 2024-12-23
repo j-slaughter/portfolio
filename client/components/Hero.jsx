9a4cdfcb83706f8f88126a4a15c577372fea94c9
Jennifer Slaughter <jslaughter@JennifesMacBook.lan>
2024-12-23 15:58:44 -0800

    create hero section

--- client/components/Hero.jsx
+++ client/components/Hero.jsx
@@ -4,11 +4,71 @@
  */
 
 import React from 'react';
+import { motion } from 'motion/react';
+import { AiOutlineGithub, AiOutlineLinkedin } from 'react-icons/ai';
+import { TypeAnimation } from 'react-type-animation';
+import wildflowers from '../assets/flowers.png';
 
 const Hero = () => {
     return (
-        <div>
-            <p>This is the hero section.</p>
+        <div className='mt-24 max-w-[1200px] mx-auto relative'>
+            <div className='grid md:grid-cols-2 mx-4 place-items-center gap-4'>
+                <motion.div 
+                initial={{opacity: 0, y: -50}} 
+                whileInView={{opacity: 1, y: 0}}
+                viewport={{once: true}}
+                transition={{duration: 2}}
+                className='text-center'>
+                    {/* Animated role describer*/}
+                    <TypeAnimation 
+                    sequence={[
+                        // Same substring will only be typed once at start
+                        '#SOFTWARE ENGINEER', 1000,
+                        '#FULLSTACK DEVELOPER', 1000,
+                        '#CREATIVE PROBLEM-SOLVER', 1000,
+                        '#WEB DEVELOPER', 1000,
+                        '#TECH PROGRAMMER', 1000,
+                        '#HELLO WORLD', 1000
+                    ]}
+                    speed={50}
+                    repeat={Infinity}
+                    className='font-semibold text-slate-300 mb-4 text-xl md:text-2xl'/>
+                    <motion.p 
+                    initial={{opacity: 0}}
+                    whileInView={{opacity: 1}}
+                    viewport={{once: true}}
+                    transition={{duration: 2, delay: 1}}
+                    className='text-slate-300 tracking-tight mb-8 text-2xl md:text-5xl'>
+                        Hello, My name is <span>Jennifer</span>.
+                    </motion.p>
+                    <motion.p 
+                    initial={{opacity: 0}}
+                    whileInView={{opacity: 1}}
+                    viewport={{once: true}}
+                    transition={{duration: 2, delay: 1.5}}
+                    className='text-slate-300 tracking-tight mb-4 text-lg md:text-xl'>
+                        Harry Potter Ispum: A week ago. Harry would have said finding a partner
+                         for a dance would be a cinch compared to taking on a Hungarian Horntail.
+                          But now that he had done the latter, and was facing the prospect of asking
+                           a girl to the ball, he thought he'd rather have another round with the dragon.
+                    </motion.p>
+                    <motion.div 
+                    initial={{opacity: 0, x: -50}} 
+                    whileInView={{opacity: 1, x: 0}}
+                    viewport={{once: true}}
+                    transition={{duration: 1, delay: 1}}>
+                        <p>Get Résumé</p>
+                    </motion.div>
+                </motion.div>
+
+                <motion.div 
+                initial={{opacity: 0, y: 50}} 
+                whileInView={{opacity: 1, y: 0}}
+                viewport={{once: true}}
+                transition={{duration: 2}}>
+                    <img src={wildflowers}/>
+                </motion.div>
+            </div>
         </div>
     );
 };
--- client/components/Skills.jsx
+++ client/components/Skills.jsx
@@ -1,9 +1,16 @@
 /**
  * @module Skills.jsx
- * @description Footer of website.
+ * @description List of tech skills.
  */
 
 import React from 'react';
+import { 
+    DiHtml5, 
+    DiCss3, 
+    DiJavascript1, 
+    DiNodejsSmall, 
+    DiReact, 
+    DiDocker } from 'react-icons/di';
 
 const Skills = () => {
     return (
